c49e4bcad6c9018849be9d9e296b01367f6afae8
guilherme <guilherme@acheipneus.com.br>
2024-08-28 00:35:16 -0300

    .gitignore

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
+<?php
+$dbPath = __DIR__ . '/banco.sqlite';
+$pdo = new PDO('sqlite:' . $dbPath);
+?>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
